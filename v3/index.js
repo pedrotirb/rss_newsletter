6036327e6c61abffa4427ab9a58827442f8d4bb5
pedubitu <pedrohbrito@me.com>
2024-06-03 10:01:01 -0300

    insert input

---
+++ v3/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Parser = require('rss-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+const parser = new Parser();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/rss', async (req, res) => {
+    let feed = await parser.parseURL(req.body.url);
+    let items = feed.items.slice(0, 10);
+
+    let html = items.map(item => `
+    <h3><a href="${item.link}">${item.title}</a></h3>
+    <p>${item.contentSnippet}</p>
+    `).join('');
+
+    res.send(html);
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
