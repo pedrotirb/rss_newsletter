a6636bb88c77fe4ae0edb7bba627f48c2f3ad517
pedubitu <pedrohbrito@me.com>
2024-06-03 14:15:24 -0300

    django installation

--- v3/index.js
+++ v3/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
+const app = express();
 const Parser = require('rss-parser');
 const bodyParser = require('body-parser');
 
-const app = express();
 const parser = new Parser();
 
-app.use(express.static('public'));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/rss', async (req, res) => {
