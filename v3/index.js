5bd8ee4f5826479cc2700e6df15c56cc8dfec731
pedubitu <pedrohbrito@me.com>
2024-06-03 13:43:14 -0300

    rearrange folders

--- v3/index.js
+++ v3/index.js
@@ -20,6 +20,10 @@ app.post('/rss', async (req, res) => {
     res.send(html);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
